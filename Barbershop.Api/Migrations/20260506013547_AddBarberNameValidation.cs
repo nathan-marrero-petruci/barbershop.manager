b9f4ae09fc153bc9ab4f5024bae2df9b10e6b276
Nathan Petruci <nathan_marrero@hotmail.com>
2026-05-05 22:35:53 -0300

    fix: migration para sincronizar snapshot após annotations no Barber

---
+++ Barbershop.Api/Migrations/20260506013547_AddBarberNameValidation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Barbershop.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBarberNameValidation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
